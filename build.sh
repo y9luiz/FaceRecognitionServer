e865ec87c9e66f7549c74fdff829246d675f1b04
Luiz Felipe M. Correia <luiz_correia@virgo.atlantico.net.br>
2022-07-02 17:50:39 -0300

    fixed conflicts

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "BUILDING PROJECT"
-mkdir build
-cd build
-cmake   -D CMAKE_CXX_COMPILER=/usr/bin/clang++  -D CMAKE_C_COMPILER=/usr/bin/clang -D CMAKE_BUILD_TYPE=Release ..
-cmake --build .
