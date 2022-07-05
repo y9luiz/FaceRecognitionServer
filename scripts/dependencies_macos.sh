7c11c80b47119c609edc8c159c787b8a903f1237
Luiz Felipe M. Correia <luiz_correia@virgo.atlantico.net.br>
2022-07-05 11:47:57 -0300

    build on macos using travis (WIP)

---
+++ scripts/dependencies_macos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "INSTALLING DEPENDENCIES"
+
+brew install -y clang
+brew install -y cmake
+brew install -y libboost-dev
+brew install -y libboost-system-dev
+brew install -y libboost-date-time-dev
+brew install -y ninja-build
