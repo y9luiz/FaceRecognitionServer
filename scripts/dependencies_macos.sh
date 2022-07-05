f44634eddc9a499b22a9fb797dc6ec4c04d69932
Luiz Felipe M. Correia <luiz_correia@virgo.atlantico.net.br>
2022-07-05 12:28:03 -0300

    removed already installed dependencies on macos

--- scripts/dependencies_macos.sh
+++ scripts/dependencies_macos.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 echo "INSTALLING DEPENDENCIES"
-brew install clang
-brew install cmake
-brew install libboost-dev
-brew install libboost-system-dev
-brew install libboost-date-time-dev
-brew install ninja-build
+brew install boost
