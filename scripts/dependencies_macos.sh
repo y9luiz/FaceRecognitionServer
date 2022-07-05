9142288f4f16b593b78b076cdbf809abab51caf1
Luiz Felipe M. Correia <luiz_correia@virgo.atlantico.net.br>
2022-07-05 12:10:03 -0300

    removed brew update and -y from dependencies on macos

--- scripts/dependencies_macos.sh
+++ scripts/dependencies_macos.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 echo "INSTALLING DEPENDENCIES"
-brew update
-brew install -y clang
-brew install -y cmake
-brew install -y libboost-dev
-brew install -y libboost-system-dev
-brew install -y libboost-date-time-dev
-brew install -y ninja-build
+brew install clang
+brew install cmake
+brew install libboost-dev
+brew install libboost-system-dev
+brew install libboost-date-time-dev
+brew install ninja-build
