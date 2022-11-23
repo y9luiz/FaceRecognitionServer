4449a7e89a74dfc5a3a2307bfce4dbda5febc3bf
Luiz Felipe <y9luiz@hotmail.com>
2022-11-22 23:11:52 -0300

    added lcov support (#26)

--- scripts/build_coverage.sh
+++ scripts/build_coverage.sh
@@ -2,5 +2,9 @@
 echo "BUILDING PROJECT AS CODE COVERAGE"
 mkdir build_coverage
 cd build_coverage
-cmake   -D CMAKE_CXX_COMPILER=/usr/bin/clang++  -D CMAKE_C_COMPILER=/usr/bin/clang -D CMAKE_BUILD_TYPE=Coverage ..
+cmake -G Ninja -D CMAKE_BUILD_TYPE=Debug -D ENABLE_CODE_COVERAGE=ON -D CMAKE_CXX_COMPILER=/usr/bin/g++  -D CMAKE_C_COMPILER=/usr/bin/gcc ..
 cmake --build .
+ctest
+lcov -c -d . -o coverage.info
+lcov -remove coverage.info '/usr/*' '*google*' '*mock*' '*test*' --output-file coverage.info
+genhtml coverage.info --output-directory=./report
