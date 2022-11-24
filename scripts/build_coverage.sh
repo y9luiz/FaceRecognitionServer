50d88aad8dc21e26d9f32e20cab14fd094aefc5c
Luiz Correia <y9luiz@hotmail.com>
2022-11-23 22:05:26 -0300

    still working

--- scripts/build_coverage.sh
+++ scripts/build_coverage.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "BUILDING PROJECT AS CODE COVERAGE"
+
 mkdir build_coverage
 cd build_coverage
 cmake -G Ninja -D CMAKE_BUILD_TYPE=Debug -D ENABLE_CODE_COVERAGE=ON -D CMAKE_CXX_COMPILER=/usr/bin/g++  -D CMAKE_C_COMPILER=/usr/bin/gcc ..
@@ -7,4 +8,4 @@ cmake --build .
 ctest
 lcov -c -d . -o coverage.info
 lcov -remove coverage.info '/usr/*' '*google*' '*mock*' '*test*' --output-file coverage.info
-genhtml coverage.info --output-directory=./report
+genhtml coverage.info --output-directory=../report
